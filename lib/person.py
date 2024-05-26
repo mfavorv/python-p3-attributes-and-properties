a2af630d2ce78059cfd99fc70e29eaed30db4890
Favor Vincent <vfavorm77@gmail.com>
2024-05-26 17:23:41 +0300

    attributes-and-properties lab complete

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,32 @@ APPROVED_JOBS = [
 ]
 
 class Person:
+    def __init__(self, name = "Keanu", job = "architect"):
+        self.job = job
+        self.name = name
+    
+    def get_name(self):
+        return self._name
+    
+    def get_job(self):
+        return self._job
+ 
+    def validity(self, name):
+       if((type(name) == str) and (1<= len(name) <=25)):
+          self._name = name.title()
+       else:
+          print("Name must be string between 1 and 25 characters.")
+    name = property(get_name, validity)
+
+    def approval(self, job):
+        if job in APPROVED_JOBS:
+            self._job = job
+        else:
+           print("Job must be in list of approved jobs." )
+    job = property(get_job, approval)
     pass
+
+rooney = Person(name="Rooney")
+rooney.validity("Rooney")
+rooney.approval("Plumber")
+
